17cf30ae3b55cfb39d48328291ebf31ebe64583a
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-02-06 16:54:43 +0000

    Created Flight Class

---
+++ src/main/java/Flight.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Flight {
+
+    private String destination;
+    private int flightID;
+    private ArrayList<Passenger> passenger;
+
+    public Flight(String destination, int flightID) {
+        this.destination = destination;
+        this.flightID = flightID;
+        ArrayList<Passenger> passenger = new ArrayList<>();
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getFlightID() {
+        return flightID;
+    }
+
+    public void setFlightID(int flightID) {
+        this.flightID = flightID;
+    }
+
+}
